129d9a4cd5ff9c33aa92f19951fa83163eb919fb
Ngan Nguyen <thienngan.nguyen1709@gmail.com>
2021-03-21 11:14:21 +0700

    ngan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
 <html>
     
 <head>
-	<title>My Awesome Login Page</title>
+	<title>Petme | Login Page</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
@@ -92,7 +92,7 @@
 			<div class="user_card">
 				<div class="d-flex justify-content-center">
 					<div class="brand_logo_container">
-						<a href="{{url('/')}}"><img src="https://cdn.freebiesupply.com/logos/large/2x/pinterest-circle-logo-png-transparent.png" class="brand_logo" alt="Logo"></a>
+						<a href="{{url('/product')}}"><img src="\Image\petme.png" class="brand_logo" alt="Logo"></a>
 					</div>
 				</div>
 				<div class="d-flex justify-content-center form_container">
