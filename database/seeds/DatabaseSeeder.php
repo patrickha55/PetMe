77d2bb3142eed92fb8510ef8f48cffc39e937e2e
Patrick Ha <pattrickha@gmail.com>
2021-04-08 09:43:53 +0700

    phat

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,31 +10,14 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run(){
-<<<<<<< HEAD
-       $this->call(LaratrustSeeder::class);
-       $this->call(UserSeeder::class);
-       $this->call(AnimalCategorySeeder::class);
-         $this->call(SupplierSeeder::class);
-         $this->call(ProductCategorySeeder::class);
-         $this->call(ProductSeeder::class);
-        $this->call(ProductDetailSeeder::class);
-         $this->call(NutritionFactSeeder::class);
-     
-=======
-        $this->call(LaratrustSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(AnimalCategorySeeder::class);
-          $this->call(SupplierSeeder::class);
-          $this->call(ProductCategorySeeder::class);
-          $this->call(ProductSeeder::class);
-         $this->call(ProductDetailSeeder::class);
->>>>>>> main
-      
-      
-  
- 
-     
-        
+      $this->call(LaratrustSeeder::class);
+      $this->call(UserSeeder::class);
+      $this->call(AnimalCategorySeeder::class);
+      $this->call(SupplierSeeder::class);
+      $this->call(ProductCategorySeeder::class);
+      $this->call(ProductSeeder::class);
+      $this->call(ProductDetailSeeder::class);
+      $this->call(NutritionFactSeeder::class);
     }
 }
 
